8e299e5fbcd18dc2b5d1302c17cbee27ed03d3cb
Daniel Rodriguez <kungary04@gmail.com>
2025-10-13 01:03:49 -0500

    Practica 2 y Clases

--- clases.py
+++ clases.py
@@ -1,38 +1,86 @@
 class Usuario:
-    def __init__(self, nombre, email):
-        print(f"Usuario creado con exito: {nombre}, {email}")
+    #atributo de clase
+    rol = "aspirante"
+
+    #deifiniendo atributos de instancia
+    def __init__(self, nombre, correo):
         self.nombre = nombre
-        self.email = email
-        self.rol = None
-        
-
-    def iniciarSesion(self):
-        print("Iniciando sesion")
-
-    def cerrarSesion(self):
-        print("Cerrando sesion")
-
-    def actualizarDatos(self, emailNuevo):
-        self.email = emailNuevo
-        print("email actualizado correctamente")
-
-class Evaluacion:
-    minutosDeEvaluacion=90
-    def __init__(self, nota, tipoDeEvaluacion, laboratorio):
-        self.nota=nota
-        self.tipoDeEvaluacion=tipoDeEvaluacion
-        self.laboratorio=laboratorio
+        self.correo = correo
     
+    #Metodo de instancia 
+    def iniciarSesion(self, correo, contraseña):
+        correoValido = "ejemplo@mail.com"
+        contraseñaValida = "contraseñasegura"
+        print("Iniciando sesion")
+        if correo==correoValido and contraseña==contraseñaValida:
+            print("Credenciales correctas")
+        else:
+            print("Credenciales incorrectas")
+#Intanciando un objeto
+print("Creando un usuario")
+usuario = Usuario("Daniel","daniel24@email.com")
+print("Ejecucion del metodo")
+usuario.iniciarSesion("ejemplo@mail.com","contraseñasegura")
+
+#Instanciando un segundo objeto
+print("Creando un usuario")
+usuario2=Usuario("Steph","steph@email.com")
+print("Ejecucion del metodo")
+usuario.iniciarSesion("ejemplo@mail.com","contraseñasegura")
+
+#Cambio de un atributo
+print(f"El usuario 2 es {usuario2.rol}")
+print("Cambio de atributo en usuario 2")
+usuario2.rol = "administrador"
+print(f"El usuario 2 ahora es {usuario2.rol}")
+        
+class Administrador:
+    pass
+class Aspirante:
+    pass
+class Evaluacion:
+    def __init__(self,nota, tiempoEvaluacion):
+        self.__nota=nota
+        self.tiempoEvaluacion= tiempoEvaluacion
+
+    #Encapsulamiento del atributo nota
+    @property #Getter
+    def nota(self):
+        return self.__nota
+    
+    @nota.setter #Setter
+    def nota(self,valor):
+        if valor != "":
+            print("Modificando valor")
+            self.__nota = valor
+        else:
+            print("El valor esta vacio")
+
     def ejecutarEvaluacion(self):
         print("Ejecutando evaluacion")
-    
-    def cancelarEvaluacion(self):
-        print("Evaluacion cancelada")
-
 class Universidad:
     def __init__(self, nombreUniversidad):
         self.nombreUniversidad=nombreUniversidad
-
+class Documento:
+    pass
+class Notificacion:
+    pass
+class Reporte:
+    pass
+class Postulacion:
+    pass
+class OfertaAcademica:
+    pass
+class Periodo:
+    pass
+class Laboratorio:
+    pass
+class Sede:
+    pass
+class Matriz:
+    pass
+class Extension:
+    pass
 class Carrera:
     pass    
 
